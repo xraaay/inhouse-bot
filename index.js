75228662297eb7f90047d68c68f93cb051506582
Ray <raymond.y.xie@gmail.com>
2020-05-14 12:02:59 -0700

    add env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { prefix, token } = require('./config.json')
+// const { prefix, token } = require('./config.json')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -7,6 +7,9 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.login(token);
+
+const prefix = process.env.PREFIX;
+const token = process.env.TOKEN;
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
