16e72d01102b81a784a6ff4d4e6cdfdf10d6e891
Ray <raymond.y.xie@gmail.com>
2020-05-17 04:24:31 -0700

    remove comments, refactor

--- commands/inhouse.js
+++ commands/inhouse.js
@@ -8,10 +8,10 @@ module.exports = {
     execute(message, args) {
         if (!message.guild) return;
 
-        // if (message.member.voice.channel) {
+        if (message.member.voice.channel) {
             handleCollectPlayers(message, args)
-        // } else {
-        //     message.channel.send("You must be in a voice channel to use this feature")
-        // }
+        } else {
+            message.channel.send("You must be in a voice channel to use this feature")
+        }
     },
 };
